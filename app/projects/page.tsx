2970b0e0e018ce81c08f4ec79c6cdc318c9c3397
mbaren135 <102633983+mbaren135@users.noreply.github.com>
2025-03-03 09:55:43 -0500

    first build

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,6 @@
 import s from "../page.module.css";
 export default function Projects() {
   return (
-    <div className={s.page}>Check back later to see what I'm working on!</div>
+    <div className={s.page}>Check back later to see what I am working on!</div>
   );
 }
