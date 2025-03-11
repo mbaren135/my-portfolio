513e7721739f66597f9cfd4310b1b388d754dced
mbaren135 <102633983+mbaren135@users.noreply.github.com>
2025-03-11 14:58:31 -0400

    add better image toggling

--- app/projects/carousel/index.tsx
+++ app/projects/carousel/index.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode, useEffect, useState } from "react";
 import Image from "next/image";
-import { ChevronLeftIcon, ChevronRightIcon } from "@/assets/icons";
+import { ChevronLeftIcon, ChevronRightIcon, CloseIcon } from "@/assets/icons";
 import { ProjectCardType } from "../../../types";
 import s from "./carousel.module.css";
 
@@ -16,6 +16,8 @@ export default function CardCarousel({
   projects: ProjectCardType[];
 }) {
   const [current, setCurrent] = useState(0);
+  const [isEnlarged, setIsEnlarged] = useState(false);
+
   useEffect(() => {
     const cards = document.querySelectorAll("." + s.card);
     if (cards.length === 0) return;
@@ -26,16 +28,21 @@ export default function CardCarousel({
       transform: string,
       zIndex: string,
       filter: string,
-      opacity: string
+      opacity: string,
+      overflow: string
     ) {
       card.style.transform = transform;
       card.style.zIndex = zIndex;
       card.style.filter = filter;
       card.style.opacity = opacity;
+
+      /** Body of the card, containing the text description of each project */
+      const cardBody = card.children[card.childElementCount - 1] as HTMLElement;
+      cardBody.style.overflowY = overflow;
     }
 
     /** Set style of the current card */
-    setStyle(cards[current] as HTMLElement, "none", "1", "none", "1");
+    setStyle(cards[current] as HTMLElement, "none", "1", "none", "1", "auto");
 
     let offset = 0;
     for (let i = current + 1; i < cards.length; i++) {
@@ -48,7 +55,8 @@ export default function CardCarousel({
         }) perspective(16px) rotateY(-1deg)`,
         `${-offset}`,
         "blur(5px)",
-        `${offset > 1 ? "0" : "0.6"}`
+        `${offset > 1 ? "0" : "0.6"}`,
+        "hidden"
       );
     }
 
@@ -63,7 +71,8 @@ export default function CardCarousel({
         }) perspective(16px) rotateY(1deg)`,
         `${-offset}`,
         "blur(5px)",
-        `${offset > 1 ? "0" : "0.6"}`
+        `${offset > 1 ? "0" : "0.6"}`,
+        "hidden"
       );
     }
   }, [current]);
@@ -73,7 +82,11 @@ export default function CardCarousel({
       {projects.map((p, index) => {
         return (
           <div key={`card-${index}`} className={s.card}>
-            <Card title={p.title} image={p.image || null}>
+            <Card
+              title={p.title}
+              image={p.image || null}
+              state={{ isEnlarged, setIsEnlarged }}
+            >
               {p.content}
             </Card>
           </div>
@@ -83,14 +96,20 @@ export default function CardCarousel({
         <>
           <button
             className={`${s.button}`}
-            onClick={() => setCurrent((prev) => prev - 1)}
+            onClick={() => {
+              setIsEnlarged(false);
+              setCurrent((prev) => prev - 1);
+            }}
             disabled={current === 0}
           >
             <ChevronLeftIcon size="30px" />
           </button>
           <button
             className={`${s.button} ${s.next}`}
-            onClick={() => setCurrent((prev) => prev + 1)}
+            onClick={() => {
+              setIsEnlarged(false);
+              setCurrent((prev) => prev + 1);
+            }}
             disabled={current === projects.length - 1}
           >
             <ChevronRightIcon size="30px" />
@@ -103,7 +122,10 @@ export default function CardCarousel({
                   className={`${s.indicator} ${
                     index === current ? s.active : ""
                   }`}
-                  onClick={() => setCurrent(index)}
+                  onClick={() => {
+                    setIsEnlarged(false);
+                    setCurrent(index);
+                  }}
                 />
               );
             })}
@@ -117,33 +139,35 @@ export default function CardCarousel({
 function Card({
   title,
   image,
+  state,
   children,
 }: {
   title: string;
   image: ReactNode | null;
+  state: { isEnlarged: boolean; setIsEnlarged: (arg: boolean) => void };
   children: ReactNode;
 }) {
-  const [isEnlarged, setIsEnlarged] = useState(false);
+  const { isEnlarged, setIsEnlarged } = state;
 
   if (isEnlarged) {
     return (
-      // <div className={s.cardContainer}>
-        <div className={s.imageEnlarged} onClick={() => setIsEnlarged(false)}>
-          <Image
-            src="/images/ATS-job-example.png"
-            alt="Michael Baren headshot"
-            width={249}
-            height={110}
-            className={s.image}
-            // onClick={() => console.log("Clicked")}
-          />
-        </div>
-      // </div>
+      <div className={s.imageEnlarged}>
+        <button className={s.closeButton} onClick={() => setIsEnlarged(false)}>
+          <CloseIcon />
+        </button>
+        <Image
+          src="/images/ATS-job-example.png"
+          alt="Michael Baren headshot"
+          width={249}
+          height={110}
+          className={s.image}
+          // onClick={() => console.log("Clicked")}
+        />
+      </div>
     );
   }
 
   return (
-    // <div className={s.cardContainer}>
     <>
       {image ? (
         <div className={s.thumbnail} onClick={() => setIsEnlarged(true)}>
@@ -153,6 +177,5 @@ function Card({
       <div className={s.title}>{title}</div>
       <div className={s.cardBody}>{children}</div>
     </>
-    // </div>
   );
 }
